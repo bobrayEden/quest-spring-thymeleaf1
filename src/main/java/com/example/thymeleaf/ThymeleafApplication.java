405f36db25dabbeb65bb57b22aa7dca86ae01716
0_Nocaudie <o.nocaudie@googlemail.com>
2019-10-08 17:29:16 +0200

    done

--- src/main/java/com/example/thymeleaf/ThymeleafApplication.java
+++ src/main/java/com/example/thymeleaf/ThymeleafApplication.java
@@ -2,6 +2,11 @@ package com.example.thymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class ThymeleafApplication {
@@ -9,5 +14,4 @@ public class ThymeleafApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafApplication.class, args);
 	}
-
 }
